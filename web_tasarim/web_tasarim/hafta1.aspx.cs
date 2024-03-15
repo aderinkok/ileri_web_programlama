efcd26321ca0e6e5fd8eaa81ee1b43eb8276c3d7
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-03-15 17:05:22 +0300

    Hafta 1

---
+++ web_tasarim/web_tasarim/hafta1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_tasarim
+{
+    public partial class hafta1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = TextBox1.Text;
+        }
+    }
+}
