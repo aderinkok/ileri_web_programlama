7f69819c922106952bf0c8bbea2bb1e907355af6
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-04-03 12:12:09 +0300

    H3

---
+++ web_tasarim/web_tasarim/hafta3.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hafta3.aspx.cs" Inherits="web_tasarim.hafta3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td><asp:TextBox runat="server" ID="SatirSayisi" placeholder="Satır Sayısı" Height="30px" Width="100px"></asp:TextBox></td>
+                <td><asp:TextBox runat="server" ID="SutunSayisi" placeholder="Sütun Sayısı" Height="30px" Width="100px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                     <asp:Button runat="server" Width="200px" ID="Olustur" Text="Tablo Oluştur" OnClick="Olustur_Click" />
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label runat="server" ID="Bilgi" Text=""></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Table runat="server" ID="Tablo1"></asp:Table>
+                </td>
+            </tr>
+        </table>
+       
+         
+       
+     </form>
+</body>
+</html>
---
+++ web_tasarim/web_tasarim/hafta3.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_tasarim
+{
+    public partial class hafta3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Olustur_Click(object sender, EventArgs e)
+        {
+            int satirS, sutunS;
+            Tablo1.BorderStyle = BorderStyle.Solid;
+            Tablo1.BorderColor = Color.Black;
+            /*
+             Textbox'lara girilen değeri kontrol et.
+            Eğer sayısal değer girilmemişse Label'da uyarı ver işleme devam etme.
+             */
+            satirS = Convert.ToInt16(SatirSayisi.Text);
+            sutunS=Convert.ToInt16(SutunSayisi.Text);
+
+            /*
+             && ve
+             || veya
+             
+             */
+            if(satirS > 20 || sutunS >20)
+            {
+                Bilgi.Text = "Satır sayısı veya sütun sayısı 20'den büyük olamaz.";
+            }
+            else
+            {
+                Bilgi.Text = "";
+
+                for(int i = 1; i <= satirS; i++)
+                {
+                    // Nesne tanımlaması
+                    // SinifAdi nesneAdi = new SinifAdi();
+                    TableRow tSatir = new TableRow();
+
+                    Tablo1.Controls.Add(tSatir);
+                    for(int j = 0; j < sutunS; j++)
+                    {
+                        TableCell hucre = new TableCell();
+                        hucre.Text = "R-"+i+" / "+"C-"+(j+1);
+                        hucre.BorderColor = Color.Black;
+                        hucre.BorderStyle = BorderStyle.Solid;
+                        hucre.BorderWidth = 1;
+                        tSatir.Controls.Add(hucre);
+
+                    }
+
+
+                }
+            }
+
+
+
+        }
+    }
+}
