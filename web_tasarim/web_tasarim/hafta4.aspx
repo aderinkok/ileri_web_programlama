852fb7bfb4c63f0af69fbe02a162b06d3b410288
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-04-16 23:35:02 +0300

    Uygulama4

---
+++ web_tasarim/web_tasarim/hafta4.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hafta4.aspx.cs" Inherits="web_tasarim.hafta4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ListBox ID="liste" runat="server" >
+           
+        </asp:ListBox>
+        <br />
+       <asp:TextBox ID="txt_metin" runat="server"></asp:TextBox> 
+        <br />
+        <asp:Button ID="btn_ekle" runat="server" Text="Ekle" OnClick="btn_ekle_Click" />
+        <asp:Button ID="btn_sil" runat="server" Text="Sil" OnClick="btn_sil_Click" />
+    </form>
+</body>
+</html>
---
+++ web_tasarim/web_tasarim/hafta4.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_tasarim
+{
+    public partial class hafta4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Response.Write("Test");
+
+           // Button2.Enabled = false;
+        }
+
+        protected void btn_ekle_Click(object sender, EventArgs e)
+        {
+            if (txt_metin.Text !="")
+            {
+                liste.Items.Add(txt_metin.Text);
+                txt_metin.Text = "";
+            }
+            
+
+        }
+
+        protected void btn_sil_Click(object sender, EventArgs e)
+        {
+            liste.Items.Remove(liste.SelectedItem);
+        }
+    }
+}
