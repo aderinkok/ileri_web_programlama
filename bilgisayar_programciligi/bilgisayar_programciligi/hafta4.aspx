32bb3be7d4e8ee8589a179ee709906d4512ed29a
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-04-18 16:28:28 +0300

    h4 / 2

---
+++ bilgisayar_programciligi/bilgisayar_programciligi/hafta4.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hafta4.aspx.cs" Inherits="bilgisayar_programciligi.hafta4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="liste" runat="server">
+         
+            </asp:ListBox>
+
+            <br />
+
+            <asp:TextBox ID="txt_isim" runat="server" ></asp:TextBox>
+            <br />
+            <asp:Button ID="btn_ekle" runat="server" Text="Ekle" OnClick="btn_ekle_Click"/>
+            <br />
+            <asp:Button ID="btn_sil" runat="server" Text="Sil" OnClick="btn_sil_Click"/>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ bilgisayar_programciligi/bilgisayar_programciligi/hafta4.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bilgisayar_programciligi
+{
+    public partial class hafta4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //  cikti.Text = "Örnek";
+
+            //Response.Write("Çıktı");
+
+               
+        }
+
+        protected void btn_ekle_Click(object sender, EventArgs e)
+        {
+            if (txt_isim.Text != "")
+            {
+                liste.Items.Add(txt_isim.Text);
+                txt_isim.Text = "";
+            }
+            
+        }
+
+        protected void btn_sil_Click(object sender, EventArgs e)
+        {
+            liste.Items.Remove(liste.SelectedItem);
+        }
+    }
+}
