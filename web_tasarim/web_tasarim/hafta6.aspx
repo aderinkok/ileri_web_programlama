097fea72aa8ec7255a50dda90a5adc051a13cef9
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-05-15 21:13:25 +0300

    H6

---
+++ web_tasarim/web_tasarim/hafta6.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hafta6.aspx.cs" Inherits="web_tasarim.hafta6" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:DropDownList runat="server" ID="il" Width="150px" AutoPostBack="True" OnSelectedIndexChanged="il_SelectedIndexChanged">
+                            <asp:ListItem Value="-1">-- İl seçin--</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:DropDownList runat="server" ID="ilce" Width="150px">
+                            <asp:ListItem Value="-1">-- İlçe seçin--</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ web_tasarim/web_tasarim/hafta6.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_tasarim
+{
+    public partial class hafta6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+               
+            
+            List<ListItem> iller = new List<ListItem>() {
+            
+            new ListItem("Bilecik","11"),
+            new ListItem("Malatya","44"),
+            new ListItem("Bursa","16"),
+
+            };
+
+            foreach(ListItem oge in iller)
+            {
+                il.Items.Add(oge);
+            }
+            }
+
+        }
+
+        protected void il_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<ListItem> ilceler = new List<ListItem>();
+            ilceler.Clear();
+            ilce.Items.Clear();
+            if (il.SelectedValue == "-1") { }
+
+            if (il.SelectedValue == "11") {
+
+                ilceler.AddRange(new List<ListItem>{
+                new ListItem("Merkez","1"),
+                new ListItem("Pazaryeri","2"),
+                new ListItem("Bozüyük","3"),
+                new ListItem("Söğüt","4"),
+                new ListItem("Osmaneli","5"),
+                new ListItem("Yenipazar","6"),
+                new ListItem("İnhisar","7"),
+                new ListItem("Gölpazarı","8"),
+                }) ;
+            
+            }
+            if (il.SelectedValue == "44") {
+                ilceler.AddRange(new List<ListItem> { 
+                
+                new ListItem("Battalgaz,","9"),
+                new ListItem("Yeşilyurt","10"),
+                new ListItem("Darende","11"),
+                new ListItem("Akçadağ","12")
+                });
+            
+            }
+            if (il.SelectedValue == "16") {
+                ilceler.AddRange(new List<ListItem> {
+                new ListItem("Yıldırım","13"),
+                new ListItem("Osmangazi","14"),
+                new ListItem("İnegöl","15"),
+                new ListItem("Nilüfer","16"),
+                new ListItem("Yenişehir","17"),
+                
+                });
+            
+                   
+            }
+
+
+            foreach(ListItem oge in ilceler)
+            {
+                ilce.Items.Add(oge);
+            }
+        }
+    }
+}
