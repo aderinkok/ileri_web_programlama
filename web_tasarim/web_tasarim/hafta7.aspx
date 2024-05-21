6d9172f30b3efd5754da1e0b23c0459c7f59187a
Abdulmelik Derinkök <abdulmelik93@msn.com>
2024-05-21 09:48:42 +0300

    H7

---
+++ web_tasarim/web_tasarim/hafta7.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hafta7.aspx.cs" Inherits="web_tasarim.hafta7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            Başlangıç Tarihi<asp:Calendar ID="baslangic_tarihi" runat="server" OnDayRender="baslangic_tarihi_DayRender"></asp:Calendar>
+            <br />
+            <br />
+            Bitiş Tarihi<br />
+            <asp:Calendar runat="server" ID="bitis_tarihi" OnDayRender="bitis_tarihi_DayRender"></asp:Calendar><br />
+
+            <asp:Button Text="İki tarih arasındaki farkı hesapla" ID="hesapla" OnClick="hesapla_Click" runat="server" />
+            <br />
+            <asp:Label runat="server" ID="cikti"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ web_tasarim/web_tasarim/hafta7.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_tasarim
+{
+    public partial class hafta7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void hesapla_Click(object sender, EventArgs e)
+        {
+            string baslangic, bitis;
+       
+            baslangic = baslangic_tarihi.SelectedDate.ToString("D");
+            bitis = bitis_tarihi.SelectedDate.ToString("D");
+
+            cikti.Text = baslangic+" ile "+bitis +" arasında "+ Convert.ToString(Math.Abs((bitis_tarihi.SelectedDate-baslangic_tarihi.SelectedDate).TotalDays))+" gün bulunmaktadır.";
+        }
+
+        protected void baslangic_tarihi_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsToday)
+            {
+                e.Cell.Text = "<a href=\""+ e.SelectUrl+"\">Bugün</a>";
+            }
+        }
+
+        protected void bitis_tarihi_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsToday)
+            {
+                if (e.Day.IsToday)
+                {
+                    e.Cell.Text = "<a href=\"" + e.SelectUrl + "\" style=\"color:#000;\">Bugün</a>";
+                }
+            }
+
+        }
+    }
+}
